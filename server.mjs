014e5f703f062c9451b6d385ad4c291a0c828117
Kamran Yousuf <kamran.yousuf@pchotels.com>
2023-07-19 03:55:39 +0500

    1st commit

---
+++ server.mjs
@@ -0,0 +1,17 @@
+
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+const __dirname = path.resolve();
+
+const app = express();
+app.use(express.json());
+
+app.use("/static", express.static(path.join(__dirname, 'static')))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Example server listening on port ${PORT}`)
+})
